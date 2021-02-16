4e08d6334d0400f60512c5cbdf7bb23176d7c01b
Alexander <boyarincev.alex@gmail.com>
2021-02-16 14:24:48 +0600

    Тестирование вывода строки на дисплей
    Строка "LCD init" вывелась на дисплей

--- scr/main.c
+++ scr/main.c
@@ -1,10 +1,12 @@
 #include "stm32f4xx.h"
+#include "LCD1602 driver.h"
 
 int n = 0;
 int main()
 {
-	void initClock();
-	void initI2c();
+	initClock();
+	initI2c();
+	initLcd();
 	
 	while(1)
 	{
@@ -16,5 +18,7 @@ void TIM3_IRQHandler(void)
 {
 	TIM3->SR &= ~TIM_SR_UIF;
 	GPIOC->ODR ^= GPIO_ODR_ODR_13;
+	sendLcdCommand(LCD_ADDR, 0b10000000);
+	sendLcdString(LCD_ADDR, "LCD init");
 	n++;
 }
